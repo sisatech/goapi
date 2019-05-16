5ff871d251673f97b46d2ffc8f24e125a410bd33
James <james.murtagh@vorteil.io>
2019-05-16 11:01:22 +1000

    Implemented 'getDefault' graphql query.

--- g.go
+++ g.go
@@ -3,6 +3,7 @@ package goapi
 import (
 	"fmt"
 
+	"github.com/machinebox/graphql"
 	"github.com/sisatech/goapi/pkg/objects"
 )
 
@@ -84,4 +85,29 @@ func (c *Client) GermConfigQuery(germ string) (*objects.VorteilConfiguration, er
 	}
 
 	return &response.GermConfig, nil
+}
+
+// GetDefaultQuery ..
+func (c *Client) GetDefaultQuery() (*objects.Defaults, error) {
+
+	req := graphql.NewRequest(`
+	query {
+		getDefault {
+			kernel
+			platform
+		}
+	}
+	`)
+
+	type responseContainer struct {
+		GetDefault objects.Defaults `json:"getDefault"`
+	}
+
+	resp := new(responseContainer)
+	err := c.client.Run(c.ctx, req, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp.GetDefault, nil
 }
